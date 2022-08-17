3a548c47f3ecdcd78309ed2ed4a97afb3d60b353
Dino Janušić <djanusic@tria.hr>
2022-08-17 14:17:48 +0200

    routes added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -10,6 +11,12 @@ import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AddTaskComponent } from './components/add-task/add-task.component';
+import { ShopComponent } from './components/shop/shop.component';
+
+const appRoutes: Routes = [
+  {path: '', component: TasksComponent},
+  {path: 'shop', component: ShopComponent}
+]
 
 @NgModule({
   declarations: [
@@ -19,8 +26,9 @@ import { AddTaskComponent } from './components/add-task/add-task.component';
     TasksComponent,
     TaskItemComponent,
     AddTaskComponent,
+    ShopComponent,
   ],
-  imports: [BrowserModule, FontAwesomeModule, HttpClientModule, FormsModule],
+  imports: [BrowserModule, FontAwesomeModule, HttpClientModule, FormsModule, RouterModule.forRoot(appRoutes,  {enableTracing: true})],
   providers: [],
   bootstrap: [AppComponent],
 })
